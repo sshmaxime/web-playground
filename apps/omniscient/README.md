1df4cba259cb6a284103b36ef78113e942f6350c
Maxime Aubanel <sshmaxime@gmail.com>
2024-10-07 15:45:49 +0200

    📄 Omniscient: Update README.md + Add ThemeToggle

--- apps/omniscient/README.md
+++ apps/omniscient/README.md
@@ -1,1 +1,21 @@
-# Omni-stats
+# Omniscient
+
+Statistics Explorer for the Omni ecosystem.
+
+https://web-playground-omniscient.vercel.app/
+
+## Get Started
+
+Go to to the root of the Monorepo.
+
+This project uses `Yarn@4.1`, you may need to install it: https://yarnpkg.com/getting-started/install.
+
+Install dependencies:
+```
+yarn
+```
+
+Run `Omniscient`:
+```
+yarn dev --filter=@web-playground/omniscient
+```
--- apps/omniscient/src/components/layouts/header.tsx
+++ apps/omniscient/src/components/layouts/header.tsx
@@ -1,10 +1,13 @@
 import { Header as HeaderPrimitive } from "@web-playground/ui/system/layouts/header";
+import { ThemeToggle } from "@web-playground/ui/system/themeToggle";
 import { H1 } from "@web-playground/ui/system/typography";
 
 const Header = () => {
 	return (
-		<HeaderPrimitive className="py-2">
+		<HeaderPrimitive className="flex justify-between py-2">
 			<H1 className="font-black tracking-tighter">Omniscient</H1>
+
+			<ThemeToggle />
 		</HeaderPrimitive>
 	);
 };
